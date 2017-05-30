38341283384ab54176e18b4079965239e6f7130b
Paresh Bhambhani <paresh.bhambhani@gmail.com>
2017-05-30 19:14:35 -0400

    Add files via upload

---
+++ DFS/Main.cpp
@@ -0,0 +1,30 @@
+// -----------------------------------------------------------------------
+/**
+File: Main.cpp
+
+Author: Paresh B.
+
+Web: pareshbhambhani.github.io
+
+Description: Main file for BFS algorithm
+
+**/
+
+// Driver program to test methods of graph class
+
+int main()
+{
+    // Create a graph given in the above diagram
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+ 
+    cout << "Following is Depth First Traversal (starting from vertex 2) \n";
+    g.DFS(2);
+ 
+    return 0;
+}
